bb4f11020d09d15c39da48ab9508321e3731e3d9
xcakeblogs <xcakeblogs@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-04-09 21:51:36 +0000

    Uploading 0.2
    git-svn-id: https://plugins.svn.wordpress.org/pinterest-pin-it-button-for-images/trunk@529274 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- xc_pinterest.php
+++ xc_pinterest.php
@@ -5,27 +5,27 @@ Plugin URI: http://www.xcake.com.br
 Description: Displays a Pin It button directly over your images
 Author: xCakeBlogs
 Author URI: http://www.xcake.com.br
-Version: 0.1
+Version: 0.2
 */
-
 define("XCPIN_PATH", WP_PLUGIN_URL . "/" . plugin_basename( dirname(__FILE__) ) . "/" );
 define("XCPIN_NAME", "Pinterest Pin It button for images");
-define("XCPIN_VERSION", "0.1");
-
-wp_enqueue_style('xc_pinterest', XCPIN_PATH.'xc_pinterest.css'); 
+define("XCPIN_VERSION", "0.2");
 
 /* 
-Special thanks:
+	Special thanks:
 
-David Cowgill for the regex:
+	David Cowgill for the regex:
 http://docs.appthemes.com/tutorials/automatically-add-rel-attribute-to-wordpress-images/
 
-Thiago Galesi (@dsracoon) and Joao Ricardo (@JoaoRicardo_RM) for the help with regex
+	Thiago Galesi (@dsracoon) and Joao Ricardo (@JoaoRicardo_RM) for the help with regex
+
 */
+
 
 
 function xcake_pinterest($content) {
 	global $post;
+
 	$posturl = urlencode(get_permalink()); //Get the post URL
 	$pindiv = '<div class="xc_pinterest">';
 	$pinurl = '<a href="http://pinterest.com/pin/create/button/?url='.$posturl.'&media=';
@@ -34,12 +34,19 @@ function xcake_pinterest($content) {
 	$pinend = '</div>';
 	$pattern = '/<img(.*?)src="(.*?).(bmp|gif|jpeg|jpg|png)"(.*?) \/>/i';
   	$replacement = $pindiv.$pinurl.'$2.$3'.$pindescription.$pinfinish.'<img$1src="$2.$3" $4 />'.$pinend;
-  	
 	$content = preg_replace( $pattern, $replacement, $content );
+	
+	//Fix the link problem
+	$newpattern = '/<a(.*?)><div class="xc_pinterest"><a(.*?)><\/a><img(.*?)\/><\/div><\/a>/i';
+	$replacement = '<div class="xc_pinterest"><a$2></a><a$1><img$3\/></a></div>';
+
+	$content = preg_replace( $newpattern, $replacement, $content );
 	return $content;
-	
-	
 }
-add_filter('the_content', 'xcake_pinterest');
+
+if (!is_admin()) {
+	wp_enqueue_style('xc_pinterest', XCPIN_PATH.'xc_pinterest.css'); 
+	add_filter('the_content', 'xcake_pinterest');
+}
 
 ?>
